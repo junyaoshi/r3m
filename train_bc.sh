987c6c08c2d8106144f29122b1f0b5e6a3f52a44
Junyao Shi <jackshi1999@gmail.com>
2022-06-12 22:25:13 -0400

    updated training script

--- train_bc.sh
+++ train_bc.sh
@@ -1,25 +1,37 @@
 export MODEL_TYPE="r3m_bc"
+export TIME_INTERVAL=10
 export LR=0.0004
-export SAVE="cv_model=${MODEL_TYPE}_lr=${LR}"
-export BATCH_SIZE=64
-export EPOCHS=300
-export NUM_WORKERS=8
+export EVAL_FREQ=2
+export SAVE_FREQ=10
 export VIS_FREQ=1000
+export EPOCHS=300
+export BATCH_SIZE=64
+export VIS_SAMPLE_SIZE=4
+export NUM_WORKERS=8
+export SAVE="cv_model=${MODEL_TYPE}_time=${TIME_INTERVAL}_lr=${LR}_batch=${BATCH_SIZE}"
 
 echo "MODEL_TYPE: ${MODEL_TYPE}"
+echo "TIME_INTERVAL: ${TIME_INTERVAL}"
 echo "LR: ${LR}"
-echo "BATCH_SIZE: ${BATCH_SIZE}"
-echo "EPOCHS: ${EPOCHS}"
-echo "NUM_WORKERS: ${NUM_WORKERS}"
+echo "EVAL_FREQ: ${EVAL_FREQ}"
+echo "SAVE_FREQ: ${SAVE_FREQ}"
 echo "VIS_FREQ: ${VIS_FREQ}"
+echo "EPOCHS: ${EPOCHS}"
+echo "BATCH_SIZE: ${BATCH_SIZE}"
+echo "VIS_SAMPLE_SIZE: ${VIS_SAMPLE_SIZE}"
+echo "NUM_WORKERS: ${NUM_WORKERS}"
 echo "SAVE: ${SAVE}"
 
 CUDA_VISIBLE_DEVICES=1 xvfb-run -a python /home/junyao/LfHV/r3m/train_bc.py \
 --model_type=${MODEL_TYPE} \
+--time_interval=${TIME_INTERVAL} \
 --lr=${LR} \
---batch_size=${BATCH_SIZE} \
---epochs=${EPOCHS} \
---num_workers=${NUM_WORKERS} \
+--eval_freq=${EVAL_FREQ} \
+--save_freq=${SAVE_FREQ} \
 --vis_freq=${VIS_FREQ} \
+--epochs=${EPOCHS} \
+--batch_size=${BATCH_SIZE} \
+--vis_sample_size=${VIS_SAMPLE_SIZE} \
+--num_workers=${NUM_WORKERS} \
 --save=${SAVE} \
 --run_on_cv_server --use_visualizer
