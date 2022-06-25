58ca2a5a6523a9622ae45d036ed5798306796f16
Junyao Shi <jackshi1999@gmail.com>
2022-06-24 20:14:51 -0400

    updated training script

--- sanity_check_bc.sh
+++ sanity_check_bc.sh
@@ -1,19 +1,24 @@
-export MODEL_TYPE="resnet4"
+export MODEL_TYPE="transfer"
+export N_BLOCKS=4
+export NET_TYPE="residual"
 export TIME_INTERVAL=20
 export LR=0.001
 export BETA=1
-export EVAL_FREQ=1
+export EVAL_FREQ=5
 export SAVE_FREQ=50
 export EPOCHS=300
 export BATCH_SIZE=5
 export VIS_SAMPLE_SIZE=5
 export TASK_VIS_SAMPLE_SIZE=0
 export NUM_WORKERS=2
-export DATETIME=06182245
-export SAVE="cv_model=${MODEL_TYPE}_time=${TIME_INTERVAL}_lr=${LR}_beta=${BETA}_date=${DATETIME}_sanity_check"
+export DATETIME=06241415
+export SAVE="sanity_check/cv_model=${MODEL_TYPE}_blocks=${N_BLOCKS}_net=${NET_TYPE}_time=${TIME_INTERVAL}_lr=${LR}\
+_beta=${BETA}_date=${DATETIME}_sanity_check"
 # export SAVE="debug"
 
 echo "MODEL_TYPE: ${MODEL_TYPE}"
+echo "N_BLOCKS: ${N_BLOCKS}"
+echo "NET_TYPE: ${NET_TYPE}"
 echo "TIME_INTERVAL: ${TIME_INTERVAL}"
 echo "LR: ${LR}"
 echo "BETA: ${BETA}"
@@ -29,6 +34,8 @@ echo "SAVE: ${SAVE}"
 
 CUDA_VISIBLE_DEVICES=1 xvfb-run -a python /home/junyao/LfHV/r3m/train_bc.py \
 --model_type=${MODEL_TYPE} \
+--n_blocks=${N_BLOCKS} \
+--net_type=${NET_TYPE} \
 --time_interval=${TIME_INTERVAL} \
 --lr=${LR} \
 --beta=${BETA} \
